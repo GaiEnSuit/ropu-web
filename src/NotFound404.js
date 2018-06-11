eedcdb10941a27c2d99958daf2db3dd2137e37a2
truong.k77 <truong.k77@gmail.com>
2018-06-11 17:08:53 -0400

    added 404 route for errors

---
+++ src/NotFound404.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NotFound404 extends Component {
+  
+  render() {
+    
+    return (
+      <div>
+        404 The Page Could Not Be Found
+      </div>
+    );
+  }
+}
+
+export default NotFound404;
