58882c42708a2db866755a5a54d0dea5e3426ce3
truong.k77 <truong.k77@gmail.com>
2018-06-23 18:46:43 -0400

    integrated twitter as news feed

--- src/home/NewsFeed.js
+++ src/home/NewsFeed.js
@@ -1,27 +1,22 @@
 import React, { Component } from 'react';
 
-import Grid from '@material-ui/core/Grid';
+// Material-UI
 import Typography from '@material-ui/core/Typography';
 
-import NewsFeedItem from './NewsFeedItem';
-
 class NewsFeed extends Component {
-  
-  newsFeedList = this.props.newsFeed.map((feedItem) => {
-    return (
-      <Grid item xs={12} key={feedItem.id}>
-        <NewsFeedItem newsFeedItem={feedItem}/>
-      </Grid>
-    )
-  })
-  
   render() {
     return (
       <div>
-        <Typography variant='display1'>News</Typography>
-        <Grid container>
-          {this.newsFeedList}
-        </Grid>
+        <Typography variant="display1">News Feed</Typography>
+        <a className="twitter-timeline" 
+          data-partner="tweetdeck" 
+          data-theme="dark" 
+          data-link-color="#E81C4F" 
+          data-chrome="noheader nofooter noborders" 
+          href="https://twitter.com/RoPU85338305/timelines/1010648304001081344?ref_src=twsrc%5Etfw"
+        >
+          RoPU Curated Timeline
+        </a>
       </div>
     );
   }
